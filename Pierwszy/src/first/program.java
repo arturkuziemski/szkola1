28c9a419a29b1d082f7d335456ab65c7b7ddcd65
s19614 <s19614@lab4l-5.pjwstk.edu.pl>
2019-02-23 16:23:10 +0100

    COMMIT

--- Pierwszy/src/first/program.java
+++ Pierwszy/src/first/program.java
@@ -3,15 +3,9 @@ package first;
 public class program {
 
 	public static void main(String[] args) {
-		Car car = new Car();
-		car.brand = "BMW";
-		car.itsNew = true;
-		car.milleage=0;
-		car.price=100000.99;
-		System.out.println("Marka:" + car.brand);
-		System.out.println("Nowy:" + car.itsNew);
-		System.out.println("Przebieg:" + car.milleage);
-		System.out.println("Cena:" + car.price);
-		System.out.println(car);
+		Car car = new Car("BMW",true,0,100000.99);
+		car.printCar();
+		System.out.println("=====> zamiana wartosci");
+		car.setMilleage(100);
 			}
 	}
